e5a9a83d4c6e3bd756c980f615745f6554ad3dfc
Rafael Uen <rafaeluen@gmail.com>
2022-04-09 01:03:11 -0300

    finished project

--- javascript.js
+++ javascript.js
@@ -1,26 +1,36 @@
 let grid = document.querySelector("#grid");
-let color = "black";
+let color = "#0000FF";
 let gridWidth = grid.offsetWidth - 10;
 let mouseDown = false;
 let click = false;
+let sizeSlider = document.querySelector("#sizeslider");
+let sizeValue = document.querySelector("#sizevalue");
+let black = document.querySelector("#black");
+let white = document.querySelector("#white");
+let random = document.querySelector("#random");
+let btnColor = document.querySelector("#btncolor");
+let colorPicker = document.querySelector("#colorpicker");
 
+function changeGridSize(gridSize) {
+  sizeValue.textContent = gridSize + " x " + gridSize;
+  createSquares(gridSize);
+}
 
-
-
-
-grid.addEventListener('mousedown', ()=> {
+grid.addEventListener("mousedown", () => {
   mouseDown = true;
 });
-document.querySelector("body").addEventListener('mouseup', ()=> {
+document.querySelector("body").addEventListener("mouseup", () => {
   mouseDown = false;
-})
+});
 
-
-function createSquares(gridSize) {
-  let square2 = grid.querySelectorAll("div");
-  square2.forEach((element) => {
+function clearSquare() {
+  grid.querySelectorAll("div").forEach((element) => {
     element.remove();
   });
+}
+
+function createSquares(gridSize) {
+  clearSquare();
   let square = [];
   let squareSize = gridWidth / gridSize;
   for (let i = 1; i <= gridSize * gridSize; i++) {
@@ -39,7 +49,9 @@ function createSquares(gridSize) {
 function changeSquareColorClick(e) {
   let squareChange = document.querySelector(`#${e.target.id}`);
   if (color === "random") {
-    squareChange.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    squareChange.style.backgroundColor = `hsl(${
+      Math.random() * 360
+    }, 100%, 50%)`;
   } else {
     squareChange.style.backgroundColor = color;
   }
@@ -49,7 +61,9 @@ function changeSquareColor(e) {
   if (mouseDown) {
     let squareChange = document.querySelector(`#${e.target.id}`);
     if (color === "random") {
-      squareChange.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      squareChange.style.backgroundColor = `hsl(${
+        Math.random() * 360
+      }, 100%, 50%)`;
     } else {
       squareChange.style.backgroundColor = color;
     }
@@ -74,7 +88,25 @@ function resetSquareColors() {
 }
 
 function changeColor(colorChange) {
+  if (colorChange === "color") {
+    color = colorPicker.value;
+  } else {
   color = colorChange;
+  }
+  black.classList.remove("btn-on");
+  white.classList.remove("btn-on");
+  random.classList.remove("btn-on");
+  btnColor.classList.remove("btn-on");
+  if (color === "white") {
+    white.classList.add("btn-on");
+  } else if (color === "black") {
+    black.classList.add("btn-on");
+  } else if (color === "random") {
+    random.classList.add("btn-on");
+  } else {
+    btnColor.classList.add("btn-on");
+  }
+  console.log(color);
 }
 
 createSquares(16);
